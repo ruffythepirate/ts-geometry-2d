589ce34b279e78a5dbe21f53b483b66012470c6c
Johan Ablett-Karlsson <johan.ellfries@gmail.com>
2020-05-21 09:15:07 +0200

    Update package.json info and improve workflow. Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from "dist/Line";
+export * from "dist/LineSegment";
+export * from "dist/Point";
+export * from "dist/Vector";
+export * from "dist/Polygon";
+export * from "dist/Matrix";
+export * from "dist/Interval";
