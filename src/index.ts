fea613e1cf80810385ffa66d7712aa859713bece
Johan Ablett-Karlsson <johan.ellfries@gmail.com>
2020-05-21 09:32:02 +0200

    Move index into src file.

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./Line";
+export * from "./LineSegment";
+export * from "./Point";
+export * from "./Vector";
+export * from "./Polygon";
+export * from "./Matrix";
+export * from "./Interval";
